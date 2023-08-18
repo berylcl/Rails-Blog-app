3571aa11a25f493c0d32a7af7b6da10706073c48
berylcl <gakiiberyl@gmail.com>
2023-08-18 16:11:29 +0100

    Added methods to update comment counters and retrieve recent comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+  after_create :increment_post_comments_counter
+
+  def increment_post_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
