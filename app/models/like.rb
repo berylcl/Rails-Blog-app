dea3bfae69236f9290637cdfe351f3b5e5ae6242
berylcl <gakiiberyl@gmail.com>
2023-08-18 16:11:59 +0100

    Enhance Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :increment_post_likes_counter
+
+  def increment_post_likes_counter
+    post.update_likes_counter
+  end
 end
