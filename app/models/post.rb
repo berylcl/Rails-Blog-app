3ffdd0a761386bdd83ae6acc9532eab1ce33eef5
berylcl <gakiiberyl@gmail.com>
2023-08-18 16:12:41 +0100

    Refined the Post model to include additional methods and relationships.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+  after_create :increment_user_posts_counter
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  def update_likes_counter
+    update(likes_counter: likes.count)
+  end
 end
