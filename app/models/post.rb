4abd49ed76cd4a78d023b2164eb685fa35fce875
berylcl <gakiiberyl@gmail.com>
2023-08-18 17:33:17 +0100

    Update app/models/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,18 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   after_create :increment_user_posts_counter
-  def increment_user_posts_counter
-    author.increment!(:posts_counter)
-  end
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
+  def increment_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
