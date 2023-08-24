66d8a47c1fe33cc4e3fb41e72d9138748293d666
berylcl <gakiiberyl@gmail.com>
2023-08-24 13:51:33 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create(
+    name: Faker::Name.name,
+    photo: Faker::Avatar.image,
+    bio: Faker::Lorem.paragraph(sentence_count: 2),
+    posts_counter: 0
+  )
+end
+
+50.times do
+  Post.create(
+    title: Faker::Lorem.sentence(word_count: 3),
+    text: Faker::Lorem.paragraph(sentence_count: 2),
+    author_id: User.all.sample.id,
+    likes_counter: 0,
+    comments_counter: 0
+  )
+end
+
+100.times do
+  Comment.create(
+    text: Faker::Lorem.paragraph(sentence_count: 2),
+    author_id: User.all.sample.id,
+    post_id: Post.all.sample.id
+  )
+end
