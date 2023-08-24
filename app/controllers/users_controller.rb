e42008b1cc98ff94252b30be012dde328d45d3d8
berylcl <gakiiberyl@gmail.com>
2023-08-24 13:50:41 +0100

    Update UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
-  def index; end
+  before_action :find_user, only: [:show]
 
-  def show; end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @three_recent_posts = @user.recent_posts
+  end
+
+  def new; end
+
+  def find_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'Users not found'
+    redirect_to users_path
+  end
 end
