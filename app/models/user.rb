5ecada5e3ff16923106ba0b1ad70646435c7ab3f
berylcl <gakiiberyl@gmail.com>
2023-09-01 10:05:04 +0100

    Adding confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   attribute :photo, :string
   attribute :bio, :text
   attribute :posts_counter, :integer, default: 0
+  attribute :confirmed_at, :datetime, default: nil
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
